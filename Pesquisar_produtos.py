38d50e14040a99edef742e7ee01ba594f4852b68
Saambrc <sambegara2@gmail.com>
2024-06-21 19:32:39 -0400

    Update Pesquisar_produtos.py

--- Pesquisar_produtos.py
+++ Pesquisar_produtos.py
@@ -1,18 +1,19 @@
-produtos = [ ]
+produtos =[]
 
-def adicionar_produtos (nome, preco, quantidade):
-    produtos.append(produtos)
+def adicionar_produtos(nome, preco, quantidade):
+  produtos.append({"nome": nome, "preco": preco, "quantidade": quantidade})
 
-adicionar_produtos('arroz',70, 10)
-adicionar_produtos("")
-adicionar_produtos("")
+adicionar_produtos('arroz', 15.00, 10)
+adicionar_produtos('feijao', 7.00, 10)
+adicionar_produtos('acucar', 8.00, 10)
 
-def pesquisar_produtos (nome):
-    cont = 1
-    for produto in produtos:
-        if nome == produtos ['nome']:
-            print(f'Item localizado {cont}: ', produtos ['nome'])
-            break
-        cont+=1
-nome = input(str('Digite o nome dp produto: '))
+def pesquisar_produtos(nome):
+  cont = 1
+  for produto in produtos:
+    if nome == produto["nome"]:
+      print(f'Item localizado {cont}: ', produto["nome"])
+      break
+    cont += 1
+
+nome = input(str('Digite o nome do produto: '))
 pesquisar_produtos(nome)
